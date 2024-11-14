0ae4bcfe29b6cc3d232473b635cf999e067fe010
bmg1203 <bmg1203@naver.com>
2024-11-15 04:41:23 +0900

    [fix]: InputView 빈 입력 처리 기능 추가
    - 입력이 없는 경우 공란을 반환합니다.

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -2,11 +2,17 @@ package calculator.view;
 
 import calculator.constants.InputPrompts;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 public class InputView {
 
     public String inputString() {
         System.out.println(InputPrompts.INPUT_PROMPTS.getPrompt());
-        return Console.readLine();
+
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException e) {
+            return "";
+        }
     }
 }
