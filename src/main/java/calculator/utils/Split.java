f3af051edaf7e8cb6626438be1e4e8da1ee38952
bmg1203 <bmg1203@naver.com>
2024-11-14 22:44:28 +0900

    [feat]: Split 커스텀 구분자 추출 기능 추가

---
+++ src/main/java/calculator/utils/Split.java
@@ -0,0 +1,30 @@
+package calculator.utils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Split {
+
+    private static final String CUSTOM_EXTRACT = ".*?//(.*?)\\\n.*";
+
+    public static List<String> customSplit(String input) {
+        input = input.replaceAll(" ", "");
+
+        Pattern pattern = Pattern.compile(CUSTOM_EXTRACT);
+        Matcher matcher = pattern.matcher(input);
+        List<String> customSpearator = new ArrayList<>();
+
+        while (matcher.find()) {
+            splitNoGap(matcher.group(2).trim(), customSpearator);
+        }
+        return customSpearator;
+    }
+
+    private static void splitNoGap(String input, List<String> customSpearator) {
+        String[] strings = input.split("");
+        Collections.addAll(customSpearator, strings);
+    }
+}
