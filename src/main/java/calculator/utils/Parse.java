0a2f51502df341e35caaa9bef6ef73cf305f9de4
bmg1203 <bmg1203@naver.com>
2024-11-15 04:15:12 +0900

    [feat]: Parse 커스텀 구분자 이스케이프 처리 추가
    - 이스케이프 해주어야 하는 문자인지 체크하고 만약 맞다면 \를 추가해준다.

--- src/main/java/calculator/utils/Parse.java
+++ src/main/java/calculator/utils/Parse.java
@@ -1,11 +1,30 @@
 package calculator.utils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Parse {
 
+    private static final String SPECIAL_CHARACTER = "[\\[\\]{}()|.*+?^$\\\\]";
+
     public static List<String> parseSeparators(List<String> separators, String input) {
-        String separator = String.join("|", separators);
+        List<String> newSeparators = addHatchForSpecialCharacter(separators);
+        String separator = String.join("|", newSeparators);
         return List.of(input.split(separator));
+    }
+
+    //이것 처리 gpt 참고함
+    private static List<String> addHatchForSpecialCharacter(List<String> separators) {
+        Pattern pattern = Pattern.compile(SPECIAL_CHARACTER);
+        List<String> newSeparators = new ArrayList<>();
+        for (String string : separators) {
+            if (pattern.matcher(string).find()) {
+                string = Pattern.quote(string);
+            }
+            newSeparators.add(string);
+        }
+
+        return newSeparators;
     }
 }
